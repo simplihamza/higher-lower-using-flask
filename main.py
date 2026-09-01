736c607e078e9f28f5541ca09aff2282fa74da99
simplihamza <simplihamza@gmail.com>
2026-09-01 03:30:42 +0300

    Use Flask's int converter to validate guess route input
    Changed the route from '/<guess>' to '/<int:guess>' so Flask rejects
    non-numeric URL segments before the view function runs, instead of
    manually calling int(guess) and risking an unhandled crash. Also
    removed the else branch in the comparison, which was unreachable
    since ==, <, and > already cover every possible outcome.

--- main.py
+++ main.py
@@ -10,22 +10,20 @@ def welcome():
             f'<img src="https://media.giphy.com/media/3o7aCSPqXE5C6T8tBC/giphy.gif" '
             f'alt="Guess a Number">')
 
-@app.route('/<guess>')
+@app.route('/<int:guess>')
 def view(guess):
-    if int(guess) == number:
+    if guess == number:
         return (f'<h1>You got me!</h1>'
                 f'<img src="https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExZDZzZDdzbXcyNDgwZGcwaXgwcmFycXJoaDIwM242d3pxNm5xZnhsaiZlcD12MV9naWZzX3NlYXJjaCZjdD1n/dLhoOYRmsJVOhKbyTK/giphy.gif" '
                 f'alt="Correct Number Guessed">')
-    elif int(guess) < number:
+    elif guess < number:
         return (f'<h1>Too low, try again!</h1>'
                 f'<img src="https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExYjg4aTQ2YXlqa21qbm45ZnRkMXJ5Ym5xZzNtdDh4d2ZnajRycWh6aSZlcD12MV9naWZzX3NlYXJjaCZjdD1n/hPPx8yk3Bmqys/giphy.gif" '
                 f'alt="Guessed number is lower, try again">')
-    elif int(guess) > number:
+    elif guess > number:
         return (f'<h1>Too high, try again!</h1>'
                 f'<img src="https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExYjg4aTQ2YXlqa21qbm45ZnRkMXJ5Ym5xZzNtdDh4d2ZnajRycWh6aSZlcD12MV9naWZzX3NlYXJjaCZjdD1n/hPPx8yk3Bmqys/giphy.gif" '
                 f'alt="Guessed number is higher, try again">')
-    else:
-        return (f'<h1>Wrong option, try again!</h1>')
 
 if __name__ == '__main__':
     app.run(debug=True)
